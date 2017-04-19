f5788de02935bcbd38941d964b9bdd8833fe263d
Mak Krnic <mak@makkrnic.com>
2017-04-19 21:44:13 +0200

    support specifying asset host for local storage
    Signed-off-by: Mak Krnic <mak@makkrnic.com>

--- README.md
+++ README.md
@@ -63,7 +63,8 @@ Arc expects certain properties to be configured at the application level:
 ```elixir
 config :arc,
   storage: Arc.Storage.S3, # or Arc.Storage.Local
-  bucket: {:system, "AWS_S3_BUCKET"} # if using Amazon S3
+  bucket: {:system, "AWS_S3_BUCKET"}, # if using Amazon S3
+  asset_host: "http://static.example.com" # or {:system, "ASSET_HOST"}
 ```
 
 Along with any configuration necessary for ExAws.
--- lib/arc/storage/local.ex
+++ lib/arc/storage/local.ex
@@ -15,11 +15,12 @@ defmodule Arc.Storage.Local do
 
   def url(definition, version, file_and_scope, _options \\ []) do
     local_path = build_local_path(definition, version, file_and_scope)
+    host = host()
 
-    url = if String.starts_with?(local_path, "/") do
-      local_path
+    url = if host == "" do
+      Path.join "/", local_path
     else
-      "/" <> local_path
+      Path.join [host, local_path]
     end
 
     url |> URI.encode()
@@ -35,5 +36,14 @@ defmodule Arc.Storage.Local do
       definition.storage_dir(version, file_and_scope),
       Arc.Definition.Versioning.resolve_file_name(definition, version, file_and_scope)
     ])
+  end
+
+  defp host do
+    host_url = Application.get_env(:arc, :asset_host, "")
+
+    case host_url do
+      {:system, env_var} when is_binary(env_var) -> System.get_env(env_var)
+      url -> url
+    end
   end
 end
